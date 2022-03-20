69813a98d30aacc9085f97c5d64d0548f2e815dd
Tan Souza <tancredo.souza@outlook.com>
2022-03-20 14:23:05 -0700

    TODO: implement GrenadeLauncher

---
+++ Source/CoopGame/Private/SGrenadeLauncher.cpp
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SGrenadeLauncher.h"
+#include "SGrenadeProjectile.h"
+
+void ASGrenadeLauncher::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ASGrenadeLauncher::Fire()
+{
+	if (ProjectileClass)
+	{
+		FVector MuzzleLocation = MeshComp->GetSocketLocation("MuzzleSocket");
+		FRotator MuzzleRotation = MeshComp->GetSocketRotation("MuzzleSocket");
+
+		FActorSpawnParameters ActorSpawnParams;
+		ActorSpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButDontSpawnIfColliding;
+
+		GetWorld()->SpawnActor<ASGrenadeProjectile>(ProjectileClass, MuzzleLocation, MuzzleRotation, ActorSpawnParams);
+	}
+}
---
+++ Source/CoopGame/Public/SGrenadeLauncher.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SWeapon.h"
+#include "SGrenadeLauncher.generated.h"
+
+/**
+ * 
+ */
+class ASGrenadeProjectile;
+UCLASS()
+class COOPGAME_API ASGrenadeLauncher : public ASWeapon
+{
+	GENERATED_BODY()
+	
+protected:
+	virtual void BeginPlay() override;
+
+	virtual void Fire() override;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Weapon")
+	TSubclassOf<ASGrenadeProjectile> ProjectileClass;
+};
