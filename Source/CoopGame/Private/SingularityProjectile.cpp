2604923dc1fbab05e90c9869b14d59588159753f
Tan Souza <tancredo.souza@outlook.com>
2022-03-29 17:10:36 -0700

    change impulse amount on projectile

--- Source/CoopGame/Private/SingularityProjectile.cpp
+++ Source/CoopGame/Private/SingularityProjectile.cpp
@@ -5,6 +5,7 @@
 #include "Components/SphereComponent.h"
 #include "Kismet/GameplayStatics.h"
 #include "BlackHole.h"
+#include "SCharacter.h"
 #include "SProjectileLauncher.h"
 
 // Sets default values
